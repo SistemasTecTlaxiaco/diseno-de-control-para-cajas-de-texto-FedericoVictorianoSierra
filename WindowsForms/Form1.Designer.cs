24d75477822568f9c0c403752b2a9f07dece5787
Federico <federicovictorianosierra@gmail.com>
2022-03-12 23:52:29 -0600

    Apariencia

--- WindowsForms/Form1.Designer.cs
+++ WindowsForms/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WindowsForms
 {
-    partial class Form1
+    partial class BackGroung
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,13 +29,40 @@ namespace WindowsForms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.class11 = new WindowsForms.Controles.Class1();
+            this.SuspendLayout();
+            // 
+            // class11
+            // 
+            this.class11.AutoSize = true;
+            this.class11.Location = new System.Drawing.Point(670, 21);
+            this.class11.MinimumSize = new System.Drawing.Size(100, 50);
+            this.class11.Name = "class11";
+            this.class11.OffBackColor = System.Drawing.Color.Gray;
+            this.class11.OffToggleColor = System.Drawing.Color.Gainsboro;
+            this.class11.OnBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.class11.OnToggleColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.class11.Size = new System.Drawing.Size(100, 50);
+            this.class11.TabIndex = 0;
+            this.class11.UseVisualStyleBackColor = true;
+            this.class11.CheckedChanged += new System.EventHandler(this.class11_CheckedChanged);
+            // 
+            // BackGroung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.class11);
+            this.Name = "BackGroung";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Controles.Class1 class11;
     }
 }
 
--- WindowsForms/Form1.cs
+++ WindowsForms/Form1.cs
@@ -10,11 +10,23 @@ using System.Windows.Forms;
 
 namespace WindowsForms
 {
-    public partial class Form1 : Form
+    public partial class BackGroung : Form
     {
-        public Form1()
+        public BackGroung()
         {
             InitializeComponent();
+        }
+
+        private void class11_CheckedChanged(object sender, EventArgs e)
+        {
+            if(class11.Checked)
+            {
+                this.BackColor = Color.DimGray;
+            }
+            else
+            {
+                this.BackColor = Color.WhiteSmoke;
+            }
         }
     }
 }
